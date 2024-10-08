06b51efda317003efe9078b9ed85ccda3d6deb0c
Paramananthan T. <UT01373@unicomtic.com>
2024-10-08 11:44:51 +0530

    Initail Commit

---
+++ BikeRentalManagementSystem/Bike.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BikeRentalManagementSystem
+{
+    internal class Bike
+    {
+        public Bike(int bikeId, string brand, string model, decimal rentalPrice)
+        {
+            this.BikeId = bikeId;
+            this.Brand = brand;
+            this.Model = model;
+            this.RentalPrice = rentalPrice;
+        }
+        public int BikeId { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public decimal RentalPrice { get; set; }
+
+        public override string ToString()
+        {
+            return $"Bike ID:{BikeId}, Bike Brand:{Brand}, Bike Model :{Model}, Rental Price :{RentalPrice}";
+        }
+
+    }
+}
---
+++ BikeRentalManagementSystem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Console.ReadLine();
+        }
+
+        public static void Menu()
+        {
+            var bike = new Bike(1, "Yamaha", "FZ", 200);
+            Console.WriteLine(bike.ToString());
+        }
+    }
+}
